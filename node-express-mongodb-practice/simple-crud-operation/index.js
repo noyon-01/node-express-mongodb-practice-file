a80fc8310f82f896c4c777824d6302cec758ebab
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-11 22:24:21 +0600

    database connect to the node js server

---
+++ node-express-mongodb-practice/simple-crud-operation/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = `mongodb://simple-crud-app:BAjlNm8zFs3XnCFG@ac-wc253qi-shard-00-00.d2j8ohj.mongodb.net:27017,ac-wc253qi-shard-00-01.d2j8ohj.mongodb.net:27017,ac-wc253qi-shard-00-02.d2j8ohj.mongodb.net:27017/?ssl=true&replicaSet=atlas-3kowuv-shard-0&authSource=admin&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("simple-crud-app");
+    const userCollection = db.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+//
+//
